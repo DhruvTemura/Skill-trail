c330ba4d3914ff6fe5b1761ae4434aae0e868d14
DhruvTemura <dhruvtemura@gmail.com>
2025-07-12 19:09:04 +0530

    app jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
-import Home from './components/Home'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Home';
+import ProfilePage from './ProfilePage';
+import Recommendations from './Recommendations';
+import LearningPath from './LearningPath';
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-50">
-      <Home></Home>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/recommendations" element={<Recommendations />} />
+          <Route path="/learning-path" element={<LearningPath />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
