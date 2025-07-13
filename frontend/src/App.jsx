4ef8e278c807cdc5323a423bf8dbca6a878becc0
DhruvTemura <dhruvtemura@gmail.com>
2025-07-13 16:49:03 +0530

    appjsx updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
+import ProfileForm from './components/ProfileForm';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path='/profile' element={<ProfileForm />} />
       </Routes>
     </div>
   );
